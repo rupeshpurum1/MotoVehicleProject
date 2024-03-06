5bd5f5af3e6af5f5937550c182db299411c06cc5
rupeshpurum1 <rupeshpurum@usf.edu>
2024-03-06 11:41:11 -0500

    Initial Commit

---
+++ Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoVehicleProject
+{
+    public class Vehicle
+    {
+        string make;
+        string type;
+        string color;
+        int speed;
+        string madeIn;
+
+        public Vehicle() 
+        {
+            this.madeIn = "USA";
+        }
+
+        //actionable item
+        public void stopVehicle()
+        {
+            this.speed = 0;
+            
+        }
+
+
+    }
+
+    }
+
